6591046f373b2ede38c74a82a9df6c5cc639519b
achrafezzaam <achraf.ezzaam@gmail.com>
2024-05-16 22:46:26 +0100

    Adding the password encryption program

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+''' Handle password encryption '''
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    ''' Salt a password hash '''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Check if the given password is valid'''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
